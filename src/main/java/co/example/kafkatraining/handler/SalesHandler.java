e5fae7fa451980e889eecd5ef55f56dc59f0fd5b
Ru-kko <verase2003.1@gmail.com>
2024-10-23 11:14:41 -0500

    style: reduce cyclometric complexity

--- src/main/java/co/example/kafkatraining/handler/SalesHandler.java
+++ src/main/java/co/example/kafkatraining/handler/SalesHandler.java
@@ -2,12 +2,12 @@ package co.example.kafkatraining.handler;
 
 import co.example.kafkatraining.jpa.entity.ItemEntity;
 import co.example.kafkatraining.jpa.repository.ItemRepository;
-import co.example.kafkatraining.schemas.InsufficientStock;
-import co.example.kafkatraining.schemas.LowStock;
-import co.example.kafkatraining.schemas.Item;
-import co.example.kafkatraining.schemas.Sale;
 import co.example.kafkatraining.producers.InsufficientStockProducer;
 import co.example.kafkatraining.producers.LowStockProducer;
+import co.example.kafkatraining.schemas.InsufficientStock;
+import co.example.kafkatraining.schemas.Item;
+import co.example.kafkatraining.schemas.LowStock;
+import co.example.kafkatraining.schemas.Sale;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -21,48 +21,40 @@ public class SalesHandler {
     private final LowStockProducer lowStockProducer;
     private final InsufficientStockProducer insufficientStockProducer;
 
-
     public void process(Sale sale) {
-
-        for (Item item: sale.items()){
-
+        for (Item item : sale.items()) {
             Optional<ItemEntity> entityOpt = repository.findById(item.id());
 
-            if (entityOpt.isPresent()) {
+            if (entityOpt.isEmpty()) {
+                continue;
+            }
 
-                ItemEntity itemEntity = entityOpt.get();
+            ItemEntity itemEntity = entityOpt.get();
 
-                try{
+            try {
+                itemEntity.decreaseQuantity(item.quantity());
+            } catch (Exception e) {
+                InsufficientStock message2 = InsufficientStock.builder()
+                        .id(item.id())
+                        .saleId(sale.saleId())
+                        .customerId(sale.customerId())
+                        .descripcion("Inventory insufficient stock for sale with %s quantity".formatted(itemEntity.getQuantity()))
+                        .build();
 
-                    itemEntity.decreaseQuantity(item.quantity());
+                insufficientStockProducer.send(message2);
+            }
 
-                } catch (Exception e) {
-                    InsufficientStock message2 = InsufficientStock.builder()
-                            .id(item.id())
-                            .saleId(sale.saleId())
-                            .customerId(sale.customerId())
-                            .descripcion("Inventory insufficient stock for sale with %s quantity".formatted(itemEntity.getQuantity()))
-                            .build();
+            repository.save(itemEntity);
 
-                    insufficientStockProducer.send(message2);
-                }
+            if (itemEntity.getQuantity() < 100) {
+                LowStock message5 = LowStock.builder()
+                        .id(item.id())
+                        .saleId(sale.saleId())
+                        .customerId(sale.customerId())
+                        .descripcion("Inventory near out of stock %s".formatted(itemEntity.getQuantity()))
+                        .build();
 
-
-                repository.save(itemEntity);
-
-
-                if (itemEntity.getQuantity() < 100 ){
-                    LowStock message5 = LowStock.builder()
-                            .id(item.id())
-                            .saleId(sale.saleId())
-                            .customerId(sale.customerId())
-                            .descripcion("Inventory near out of stock %s".formatted(itemEntity.getQuantity()))
-                            .build();
-
-                    lowStockProducer.send(message5);
-                }
-
-
+                lowStockProducer.send(message5);
             }
         }
     }
