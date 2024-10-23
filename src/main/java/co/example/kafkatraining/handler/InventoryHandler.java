9207b69dee397247e81f94e1323645b57e957d9c
Ru-kko <verase2003.1@gmail.com>
2024-10-23 15:28:06 -0500

    feat: enhance create method to handle existing items and notify on high stock

--- src/main/java/co/example/kafkatraining/handler/InventoryHandler.java
+++ src/main/java/co/example/kafkatraining/handler/InventoryHandler.java
@@ -1,7 +1,10 @@
 package co.example.kafkatraining.handler;
 
+import co.example.kafkatraining.config.ApplicationProperties;
 import co.example.kafkatraining.jpa.entity.ItemEntity;
 import co.example.kafkatraining.jpa.repository.ItemRepository;
+import co.example.kafkatraining.producers.HighStockProducer;
+import co.example.kafkatraining.schemas.HighStock;
 import co.example.kafkatraining.schemas.Item;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -12,18 +15,31 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class InventoryHandler {
     private final ItemRepository itemRepository;
+    private final ApplicationProperties props;
+    private final HighStockProducer highStockProducer;
 
-    public ItemEntity create(Item item) {
-        var toSave = new ItemEntity(item.id(), item.quantity());
-        return itemRepository.save(toSave);
-    }
 
-    public void  delete(String id) {
-        if (id == null) {
-            log.warn("Cannot delete null item");
-            return;
+    public void create(Item item) {
+        var saved = itemRepository.findById(item.id());
+
+        var toSave = saved.orElse(new ItemEntity(item.id(), item.quantity()));
+
+        if (saved.isPresent()) {
+            toSave.incrementQuantity(toSave.getQuantity());
+            log.info("Updating an existing item {}", item.id());
         }
 
-        itemRepository.deleteById(id);
+        var result = this.itemRepository.save(toSave);
+
+        if (result.getQuantity() > props.getHighStock()) {
+            log.warn("item {} is exceeding the recommended stock, {}", result.getItemId(), result.getQuantity());
+            HighStock message = HighStock.builder()
+                    .id(result.getItemId())
+                    .quantity(result.getQuantity())
+                    .description("item is exceeding the recommended stock")
+                    .gap(item.quantity() - props.getHighStock())
+                    .build();
+            highStockProducer.send(message);
+        }
     }
 }
